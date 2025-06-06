6da5601ae3432ed8f3555c8371db82b28933378c
Dariusz Filipiak <darekf77@gmail.com>
2025-06-06 12:07:14 +0200

    a

---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  imports: [CommonModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  standalone: true
+})
+export class LayoutComponent {
+
+}
